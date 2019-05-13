968861785896771b087442d4a30050be4c58f399
Wesley <cricket20096@gmail.com>
2019-05-13 15:45:24 -0400

    Initial commit for slider project

---
+++ imageslide/index.js
@@ -0,0 +1,9 @@
+const nextImg = document.querySelector('.nextBtn')
+const prevImg = document.querySelector('.prevBtn')
+
+nextImg.addEventListener('click', nextSlide)
+prevImg.addEventListener('click', prevSlide)
+
+function nextSlide () {
+    
+}
