2ce82d0370e8e3d5c1eb8e6174ea01e443b1d3f7
Wesley <wesley.bonneville@gmail.com>
2019-05-20 12:04:14 -0400

    commit objects

--- store/index.js
+++ store/index.js
@@ -11,30 +11,31 @@ const cartContent = document.querySelector('.cart-content')
 const productsDOM = document.querySelector('.products-center')
 // cart 
 let cart = [];
-
 // getting the products
 class Products { // setting the class
     async getProducts() { // async await & method 
         try {
         let result = await fetch("products.json") // fetching the products from products.json & waiting for whatever we're getting back
         let data = await result.json();
-            return data;
+            let products = data.items;
+            products = products.map(item =>{
+                const {title, price} = item.fields
+                const {id} = item.sys
+                const image = item.fields.image.fields.file.url
+                return {title, price, id, image}
+            })
+            return products
         } catch (error) {
             console.log(error);
         } 
     }
 }
-
 // display products
 class UI {
-
 }
-
 // local storage
 class Storage {
-
 }
-
 document.addEventListener('DOMContentLoaded', () => { // event listening for DOM content to load
 const ui = new UI();
 const products = new Products();
