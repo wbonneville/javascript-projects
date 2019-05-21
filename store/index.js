46f41ee46ec408e95695efe0cc6f0de463823d3b
Wesley <wesley.bonneville@gmail.com>
2019-05-21 12:33:42 -0400

    Add buttons

--- store/index.js
+++ store/index.js
@@ -11,6 +11,8 @@ const cartContent = document.querySelector('.cart-content')
 const productsDOM = document.querySelector('.products-center')
 // cart 
 let cart = [];
+// buttons
+let buttonsDOM = []
 // getting the products
 class Products { // setting the class
     async getProducts() { // async await & method 
@@ -56,17 +58,47 @@ class UI {
         });
         productsDOM.innerHTML = result;
     }
+    getBagButtons(){
+        const buttons = [...document.querySelectorAll(".bag-btn")];
+        buttonsDOM = buttons;
+        buttons.forEach(button => {
+            let id = button.dataset.id;
+            let inCart = cart.find(item => item.id === id)
+                if(inCart){
+                    button.innerText = "In Cart"
+                    button.disabled = true;
+                } 
+                    button.addEventListener('click', (event)=> {
+                        event.target.innerText = "In Cart";
+                        event.target.disabled = true;
+                        // get product from products 
+                        // add product to the cart
+                        // save cart in local storage
+                        // set cart values
+                        // display cart item
+                        // show the cart
+                    })
+                }
+        })
+    }
 }
 // local storage
 class Storage {
+    static saveProducts(products){
+        localStorage.setItem("products", JSON.stringify(products))
+    }
 }
 document.addEventListener('DOMContentLoaded', () => { // event listening for DOM content to load
 const ui = new UI();
 const products = new Products();
 
 // get all products
-products.getProducts().then(products => 
-    ui.displayProducts(products))
+products.getProducts().then(products => {
+    ui.displayProducts(products);
+    Storage.saveProducts(products);
+    }).then(()=>{
+        ui.getBagButtons();
+    }) 
 });
 
 
