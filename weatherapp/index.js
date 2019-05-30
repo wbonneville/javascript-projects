029ee0f3b05ef8e9dcdf8152567f94c9c189585b
Wesley <wesley.bonneville@gmail.com>
2019-05-30 14:26:44 -0400

    Add skycons

--- weatherapp/index.js
+++ weatherapp/index.js
@@ -6,15 +6,24 @@ if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(position => {
         long = position.coords.longitude;
         lat = position.coords.latitude;
+        let temperatureDescription = document.querySelector('.temperature-description')
+        let temperatureDegree = document.querySelector('.temperature-degree')
+        let locationTimezone = document.querySelector('.location-timezone')
 
-        const api = ``;
+
+        const proxy = "https://cors-anywhere.herokuapp.com/"
+        const api = `${proxy}https://api.darksky.net/forecast/235fef3261e06fe55525dd9684d91f17/${lat},${long}`;
 
         fetch(api)
         .then(response => {
             return response.json();
         })
         .then(data => {
-            console.log(data);
+            const { temperature, summary } = data.currently 
+            //Set DOM Elements from the API
+            temperatureDegree.textContent = temperature;
+            temperatureDescription.textContent = summary;
+            locationTimezone.textContent = data.timezone
         })
     });
     }
