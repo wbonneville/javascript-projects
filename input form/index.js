bff63ddc44b9bd2f0fc01e23ebe14b93fa8abdc1
Wesley <wesley.bonneville@gmail.com>
2019-06-03 11:51:08 -0400

    HTML for input button

---
+++ input form/index.js
@@ -0,0 +1,5 @@
+var inputName = document.getElementById('input-name')
+var inputMail = document.getElementById('input-mail')
+var inputMessage = document.getElementById('input-message')
+var button = document.querySelector('submit-button')
+
