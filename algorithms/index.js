023a9bf9d1ad00188f0d4f5d4e72ef2d38aaf2fe
Wesley <wesley.bonneville@gmail.com>
2019-06-05 10:26:24 -0400

    Add largest number function

--- algorithms/index.js
+++ algorithms/index.js
@@ -57,7 +57,6 @@ function factorialize(num) {
 // End of factorialize
 // Find longest word
 
-
 var longStringInput = document.getElementById('longString');
 var checkLongestWord = document.getElementById('checkLongestWord');
 var longestWord = document.getElementById('longestWord');
@@ -78,6 +77,27 @@ function findLongestWordLength(str) {
       maxLength = words[i].length;
     }
   }
+
 return maxLength;
+
+}
+
+// End longest word
+// Largest number
+
+function largestOfFour(arr) {
+  var results = [];
+  for (var n = 0; n < arr.length; n++) {
+    var largestNumber = arr[n][0];
+    for (var sb = 1; sb < arr[n].length; sb++) {
+      if (arr[n][sb] > largestNumber) {
+        largestNumber = arr[n][sb];
+      }
+    }
+    results[n] = largestNumber;
+  }
+
+return results;
+
 }
 
