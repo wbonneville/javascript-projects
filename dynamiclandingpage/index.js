06254219f1b14b942b6d9e377d40130d903ffc05
Wesley <wesley.bonneville@gmail.com>
2019-06-19 11:35:55 -0400

    add times of day

---
+++ dynamiclandingpage/index.js
@@ -0,0 +1,61 @@
+// DOM
+
+const time = document.getElementById('time'),
+    greeting = document.getElementById('greeting'),
+    name = document.getElementById('name'),
+    focus = document.getElementById('focus');
+
+// Time
+
+function showTime() {
+    let today = new Date(),
+        hour = today.getHours(),
+        min = today.getMinutes(),
+        sec = today.getSeconds();
+
+    // AM or PM
+
+    const amPM = hour >= 12 ? 'PM' : 'AM';
+
+    // 12hr Format
+    hour = hour % 12 || 12;
+
+    // Output time
+
+    time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)}`;
+
+    setTimeout(showTime, 1000)
+}
+
+// Add zero
+
+function addZero(n) {
+    return (parseInt(n, 10) < 10 ? '0' : '') + n;
+}
+
+// Set background & greeting
+
+function setBgGreet() {
+    let today = new Date(),
+        hour = today.getHours();
+
+    if (hour < 12) {
+        // morning
+        document.body.style.backgroundImage = "url('../img/morning.jpg"
+        greeting.textContent = "Good Morning"
+    } else if (hour < 18) {
+        // afternoon
+        document.body.style.backgroundImage = "url('../img/afternoon.jpg"
+        greeting.textContent = "Good Afternoon"
+    } else {
+        // evening
+        document.body.style.backgroundImage = "url('../img/night.jpg"
+        greeting.textContent = "Good Evening"
+        document.body.style.color = 'white';
+    }
+}
+
+// Run
+
+showTime();
+setBgGreet()
